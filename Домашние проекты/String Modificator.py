252af343880769c17a9507f7ca136133a3721276
Олег <oleg.martynov2008@gmail.com>
2023-05-30 17:13:28 +0300

    Модификатор строки 1.0

--- Домашние проекты/String Modificator.py
+++ Домашние проекты/String Modificator.py
@@ -9,3 +9,36 @@
           Действия происходят сразу после выбора пункта. Все эффекты накладываются друг на друга.
           Желательно оформлять окно в ООП стиле.
 """
+from tkinter import Tk, Checkbutton, IntVar, StringVar
+from tkinter.messagebox import showinfo
+from tkinter.ttk import Entry
+window = Tk()
+window.title("Главное окно")
+window.geometry("300x200")
+
+entry = Entry()
+
+v1 = IntVar()
+v2 = IntVar()
+v3 = IntVar()
+v4 = IntVar()
+# Дополнительная возможность изменять метку checkbutton-а
+on = 'Включено'
+off = 'Отключено'
+switcher = StringVar(value=on)
+
+
+# Привязка события и переменной к checkbutton
+check1 = Checkbutton(text='Все слова с большой буквы', variable=v1)
+check2 = Checkbutton(text='Увеличить пробелы между словами', variable=v2)
+check3 = Checkbutton(text='Развернуть строку', variable=v3)
+check4 = Checkbutton(text='Удалить все значения', variable=v4)
+
+entry.pack()
+check1.pack()
+check2.pack()
+check3.pack()
+check4.pack()
+
+window.mainloop()
+
